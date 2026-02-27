1737e37dc3a3aabfc387cbc7938a69d5a7068905
phils301-cloud <phils301@gmail.com>
2026-02-28 00:28:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint, ChatHuggingFace
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
 # ────────────────────────────────────────────────
